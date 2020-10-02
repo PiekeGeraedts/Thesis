bf0d1c4d7a68dbf4a9c870371bc1e4f842b70e7b
Pieke <piekegeraedts@hotmail.com>
2020-10-02 11:48:26 +0200

    nothing much

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='pygcn',
+setup(name='pygcn_kemeny',
       version='0.1',
-      description='Graph Convolutional Networks in PyTorch',
-      author='Thomas Kipf',
-      author_email='thomas.kipf@gmail.com',
-      url='https://tkipf.github.io',
-      download_url='https://github.com/tkipf/pygcn',
+      description='Graph Convolutional Networks with Kemeny in PyTorch',
+      author='Pieke Geraedts',
+      author_email='piekegeraedts@hotmail.com',
+      url='',
+      download_url='https://github.com/Pieke/pygcn',
       license='MIT',
       install_requires=['numpy',
                         'torch',
